f911ba440da38e6264a419305fb118ee2947e2d5
Bindushree Nagaraju <bindushreenagaraju5@gmail.com>
2025-12-08 22:41:16 -0800

    Updated redis acess for prod

--- redis_index.py
+++ redis_index.py
@@ -4,6 +4,14 @@ import json
 
 r = redis.Redis(host="localhost", port=6379, db=0)
 
+from upstash_redis import Redis
+
+UPSTASH_REDIS_REST_URL = "https://communal-elf-7735.upstash.io"
+UPSTASH_REDIS_REST_TOKEN = "AR43AAImcDI3N2Q3Njk0ZDdkMmY0Yjg3OTk3MzYwODA3NzFmY2JlNHAyNzczNQ"
+
+r = Redis(url=UPSTASH_REDIS_REST_URL, token=UPSTASH_REDIS_REST_TOKEN)
+
+
 def tokenize(text):
     text = text.lower()
     tokens = re.findall(r'\b\w+\b', text)
