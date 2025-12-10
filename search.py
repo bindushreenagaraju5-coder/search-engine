9c3111b5c115b6a43bf46f95f698403de92d1282
bindushreenagaraju5-coder <bindushreenagaraju5@gmail.com>
2025-12-09 23:47:28 -0800

    Update search.py

--- search.py
+++ search.py
@@ -2,20 +2,15 @@ import json
 from flask import jsonify
 import redis
 import requests
-from redis_index import index_to_redis, tokenize
+from redis_index import UPSTASH_REDIS_REST_TOKEN, UPSTASH_REDIS_REST_URL, index_to_redis, tokenize
+from upstash_redis import Redis
 
 
 DATA_URL = "https://november7-730026606190.europe-west1.run.app/messages"
 data_source = []
 #r = redis.Redis(host="localhost", port=6379, db=0)
 
-from upstash_redis import Redis
-
-UPSTASH_REDIS_REST_URL = "https://communal-elf-7735.upstash.io"
-UPSTASH_REDIS_REST_TOKEN = "AR43AAImcDI3N2Q3Njk0ZDdkMmY0Yjg3OTk3MzYwODA3NzFmY2JlNHAyNzczNQ"
-
 r = Redis(url=UPSTASH_REDIS_REST_URL, token=UPSTASH_REDIS_REST_TOKEN)
-
 
 def load_data():
     resp = requests.get(DATA_URL)
@@ -30,36 +25,24 @@ def search(query,page,limit):
     if not query:
         return jsonify({"error": "q is required"}), 400
     
-
     tokens = tokenize(query)
-    print(tokens,'###########tokens')
 
     redis_keys = [f"token:{t}" for t in tokens]
-
-    print(redis_keys,'###########redis_keys')
     
     if r.dbsize() == 0:
         print("Loading and indexing data into Redis…")
         load_data()
-    print(r.sinter(redis_keys),'r.sinter(redis_keys)')
-    #matching_ids = r.sinter(redis_keys)
-    matching_ids = r.sinter(*redis_keys)
     
-
-    matching_ids = sorted(list(matching_ids))
-    print("matching_ids", matching_ids)
+    matching_ids = sorted(list(r.sinter(*redis_keys)))
     start = (page - 1) * limit
     end = start + limit
     page_ids = matching_ids[start:end]
-    print("page_ids", page_ids)
 
     results = []
     for doc_id in page_ids:
         raw = r.get(f"message:{doc_id}")
         if raw:
-            print(raw, '#########raw')
             doc = json.loads(raw)
-            #doc = json.loads(raw.decode('utf-8'))
             tmp = {
                 'message': doc['message'],
                 'user_name': doc['user_name']
